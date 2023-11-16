e69e2aef656098f77437b6389d700088e53bcb99
Ivan Baldiviezo <ivanbaldiviezo16@gmail.com>
2023-11-16 03:00:50 -0300

    se resuelve el ejercicio 6

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -12,69 +12,71 @@ generaDNI(): genera un número aleatorio de 8 cifras.
 */
 
 class Persona {
-    constructor(nombre, edad, sexo, peso, altura, añoNacimiento) {
-      this.nombre = nombre;
-      this.edad = edad;
-      this.DNI = this.generaDNI();
-      this.sexo = sexo;
-      this.peso = peso;
-      this.altura = altura;
-      this.añoNacimiento = añoNacimiento;
-    }
-  
-    // Método para mostrar la generación
-    mostrarGeneracion() {
-      const generacion = this.calcularGeneracion();
-      document.write(`<p>${this.nombre} pertenece a la generación ${generacion}.</p>`);
-    }
-  
-    // Método para calcular la generación basada en el año de nacimiento
-    calcularGeneracion() {
-      if (this.añoNacimiento >= 1930 && this.añoNacimiento <= 1948) {
-        return 'Silent Generation';
-      } else if (this.añoNacimiento >= 1949 && this.añoNacimiento <= 1968) {
-        return 'Baby Boom';
-      } else if (this.añoNacimiento >= 1969 && this.añoNacimiento <= 1980) {
-        return 'Generation X';
-      } else if (this.añoNacimiento >= 1981 && this.añoNacimiento <= 1993) {
-        return 'Generation Y (millennials)';
-      } else if (this.añoNacimiento >= 1994 && this.añoNacimiento <= 2010) {
-        return 'Generation Z';
-      } else {
-        return 'Generación no definida';
-      }
-    }
-  
-    // Método para verificar si es mayor de edad
-    esMayorDeEdad() {
-      return this.edad >= 18;
-    }
-  
-    // Método para mostrar todos los datos del objeto
-    mostrarDatos() {
-      document.write(`<h2>Datos de ${this.nombre}</h2>`);
-      document.write(`<p>Nombre: ${this.nombre}</p>`);
-      document.write(`<p>Edad: ${this.edad} años</p>`);
-      document.write(`<p>DNI: ${this.DNI}</p>`);
-      document.write(`<p>Sexo: ${this.sexo}</p>`);
-      document.write(`<p>Peso: ${this.peso} kg</p>`);
-      document.write(`<p>Altura: ${this.altura} cm</p>`);
-      document.write(`<p>Año de Nacimiento: ${this.añoNacimiento}</p>`);
-      document.write(`<p>Es mayor de edad: ${this.esMayorDeEdad() ? 'Sí' : 'No'}</p>`);
-      this.mostrarGeneracion();
-    }
-  
-    // Método para generar un DNI aleatorio de 8 cifras
-    generaDNI() {
-      return Math.floor(Math.random() * 90000000) + 10000000;
+  constructor(nombre, edad, sexo, peso, altura, añoNacimiento) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.DNI = this.generaDNI();
+    this.sexo = sexo;
+    this.peso = peso;
+    this.altura = altura;
+    this.añoNacimiento = añoNacimiento;
+  }
+
+  // Método para mostrar la generación
+  mostrarGeneracion() {
+    const generacion = this.calcularGeneracion();
+    document.write(
+      `<p>${this.nombre} pertenece a la generación ${generacion}.</p>`
+    );
+  }
+
+  // Método para calcular la generación 
+  calcularGeneracion() {
+    if (this.añoNacimiento >= 1930 && this.añoNacimiento <= 1948) {
+      return "Silent Generation";
+    } else if (this.añoNacimiento >= 1949 && this.añoNacimiento <= 1968) {
+      return "Baby Boom";
+    } else if (this.añoNacimiento >= 1969 && this.añoNacimiento <= 1980) {
+      return "Generation X";
+    } else if (this.añoNacimiento >= 1981 && this.añoNacimiento <= 1993) {
+      return "Generation Y (millennials)";
+    } else if (this.añoNacimiento >= 1994 && this.añoNacimiento <= 2010) {
+      return "Generation Z";
     }
   }
-  
-  // Ejemplo de uso de la clase Persona
-  const persona1 = new Persona('Juan', 25, 'H', 70, 175, 1997);
-  const persona2 = new Persona('Ana', 30, 'M', 60, 160, 1992);
-  
-  document.write('<h1>Información de Personas</h1>');
-  persona1.mostrarDatos();
-  document.write('------------------------<br>');
-  persona2.mostrarDatos();
+
+  // Método para verificar si es mayor de edad
+  esMayorDeEdad() {
+    return this.edad >= 18;
+  }
+
+  // Método para mostrar datos del objeto
+  mostrarDatos() {
+    document.write(`<h2>Datos de ${this.nombre}</h2>`);
+    document.write(`<p>Nombre: ${this.nombre}</p>`);
+    document.write(`<p>Edad: ${this.edad} años</p>`);
+    document.write(`<p>DNI: ${this.DNI}</p>`);
+    document.write(`<p>Sexo: ${this.sexo}</p>`);
+    document.write(`<p>Peso: ${this.peso} kg</p>`);
+    document.write(`<p>Altura: ${this.altura} cm</p>`);
+    document.write(`<p>Año de Nacimiento: ${this.añoNacimiento}</p>`);
+    document.write(
+      `<p>Es mayor de edad: ${this.esMayorDeEdad() ? "Sí" : "No"}</p>`
+    );
+    this.mostrarGeneracion();
+  }
+
+  // Método para generar un DNI aleatorio
+  generaDNI() {
+    return Math.floor(Math.random() * 90000000) + 10000000;
+  }
+}
+
+// Ejemplo en Persona
+const persona1 = new Persona("Juan", 25, "H", 70, 175, 1997);
+const persona2 = new Persona("Ana", 30, "M", 60, 160, 1992);
+
+document.write("<h1>Información de Personas</h1>");
+persona1.mostrarDatos();
+document.write("------------------------<br>");
+persona2.mostrarDatos();
--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -12,3 +12,71 @@ Crear sus respectivos métodos get y set correspondientes para cada propiedad. C
 Crear al menos 2 objetos libros y utilizar el método mostrarLibro();
 Por último, indicar cuál de los 2 objetos “libros” tiene más páginas.
 */
+
+class Libro {
+    constructor(ISBN, titulo, autor, numPaginas) {
+      this._ISBN = ISBN;
+      this._titulo = titulo;
+      this._autor = autor;
+      this._numPaginas = numPaginas;
+    }
+  
+    // Métodos get y set para ISBN
+    get ISBN() {
+      return this._ISBN;
+    }
+  
+    set ISBN(nuevoISBN) {
+      this._ISBN = nuevoISBN;
+    }
+  
+    // Métodos get y set para Título
+    get titulo() {
+      return this._titulo;
+    }
+  
+    set titulo(nuevoTitulo) {
+      this._titulo = nuevoTitulo;
+    }
+  
+    // Métodos get y set para Autor
+    get autor() {
+      return this._autor;
+    }
+  
+    set autor(nuevoAutor) {
+      this._autor = nuevoAutor;
+    }
+  
+    // Métodos get y set para Número de Páginas
+    get numPaginas() {
+      return this._numPaginas;
+    }
+  
+    set numPaginas(nuevoNumPaginas) {
+      this._numPaginas = nuevoNumPaginas;
+    }
+  
+    // Método para mostrar libro
+    mostrarLibro() {
+      document.write(`El libro ${this.titulo} con ISBN ${this.ISBN} creado por el autor ${this.autor} tiene ${this.numPaginas} páginas.<br>`);
+    }
+  }
+  
+  // Crear objetos libros
+  const libro1 = new Libro('123456789', 'El Señor de los Anillos', 'J.R.R. Tolkien', 1000);
+  const libro2 = new Libro('987654321', 'Harry Potter', 'J. K. Rowling', 500);
+  
+  //información de los libros
+  document.write('<h1>Información de Libros</h1>');
+  libro1.mostrarLibro();
+  libro2.mostrarLibro();
+  
+  // libro que tiene más páginas
+  if (libro1.numPaginas > libro2.numPaginas) {
+    document.write('<p>El libro 1 tiene más páginas que el libro 2.</p>');
+  } else if (libro1.numPaginas < libro2.numPaginas) {
+    document.write('<p>El libro 2 tiene más páginas que el libro 1.</p>');
+  } else {
+    document.write('<p>Ambos libros tienen la misma cantidad de páginas.</p>');
+  }
